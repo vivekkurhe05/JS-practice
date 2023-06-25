10490b8a121f0c8f9a7d6fba11234b19f121057a
Vivek_Ennotech <vivek_ennotech@Administrators-MacBook-Pro.local>
2023-06-25 10:17:11 +0530

    python dict

---
+++ Objects/python-dict/exercise17.js
@@ -0,0 +1,27 @@
+/**
+ * Write a Python program to remove duplicates from the dictionary.
+ */
+
+// work on this
+let student_data = {
+    'id1': 
+   {'name': ['Sara'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+ 'id2': 
+  {'name': ['David'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+ 'id3': 
+    {'name': ['Sara'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+ 'id4': 
+   {'name': ['Surya'], 
+    'class': ['V'], 
+    'subject_integration': ['english, math, science']
+   },
+}
---
+++ Objects/python-dict/exercise19.js
@@ -0,0 +1,13 @@
+/**
+ * Write a Python program to combine two dictionary by adding values for common keys.
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+Sample output: Counter({'a': 400, 'b': 400, 'd': 400, 'c': 300})
+ */
+
+// work on this
+let d1 = {'a': 100, 'b': 200, 'c':300}
+let d2 = {'a': 300, 'b': 200, 'd':400}
+let d3 = {}
+
+
