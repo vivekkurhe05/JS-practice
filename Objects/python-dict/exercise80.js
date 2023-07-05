f01ba24a8f9f50e88ba4a6f216302bebbc580b56
Vivek_Ennotech <vivek_ennotech@Administrators-MacBook-Pro.local>
2023-07-05 23:17:00 +0530

    python objects exercises

---
+++ Objects/python-dict/exercise80.js
@@ -0,0 +1,17 @@
+/**
+ * Write a Python program to find the key of the maximum value in a dictionary.
+Sample Output:
+Original dictionary elements:
+{'Theodore': 19, 'Roxanne': 22, 'Mathew': 21, 'Betty': 20}
+Finds the key of the maximum and minimum value of the said dictionary:
+('Roxanne', 'Theodore')
+ */
+
+// work on this
+let obj = {'Theodore': 19, 'Roxanne': 22, 'Mathew': 21, 'Betty': 20}
+let arr = []
+for (let key in obj) {
+
+}
+
+console.log(arr)
